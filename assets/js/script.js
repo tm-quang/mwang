bdbb5d4f71b8810604b652cd43a9e497ed7e249a
mwang <88404839+tm-quang@users.noreply.github.com>
2025-06-19 11:42:56 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // =================================================================================
 
 // DÁN URL CỦA WEB APP BẠN ĐÃ DEPLOY TỪ GOOGLE APPS SCRIPT VÀO ĐÂY
-const API_URL = "https://script.google.com/macros/s/AKfycbyi7WsRWJ8LPaGQrrZbGEYpClb9emajgv1feJERgDbiueLjI87uOa-0e-GYb01Xnx_Dlg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyx_0kcaqShLixhgBtvebIqVte6jbHl3qeZzvn3iL_M8fhSNIyzcRAWnWTIP-VvOx5fhQ/exec";
 
 // Cấu hình menu
 const leftMenuData = [
